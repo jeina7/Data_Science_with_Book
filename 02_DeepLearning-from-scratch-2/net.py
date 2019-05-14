48a85e3d70e0a49f086edfe3c64bc9183a7e6d01
jeina7 <jane1231001@gmail.com>
2019-05-14 18:01:24 +0900

    typo edit

--- 02_DeepLearning-from-scratch-2/net.py
+++ 02_DeepLearning-from-scratch-2/net.py
@@ -5,7 +5,7 @@
 # 2) 모든 Net은 그 안에 포함된 layers의 params를 모두 합친 Net의 새로운 변수 params를 가진다
 
 import numpy as np
-from layer import Sigmoid, Affine
+from layer import Sigmoid, Affine, SoftmaxWithLoss
 
 
 class TwoLayerNet:
@@ -25,12 +25,30 @@ class TwoLayerNet:
             Affine(W2, b2)
         ]
 
+        # loss layer
+        self.loss_layer = SoftmaxWithLoss()
+
         # collect params
-        self.params = []
+        self.params, self.grads = [], []
         for layer in self.layers:
             self.params += layer.params
+            self.grads += layer.grads
+
 
     def predict(self, x):
         for layer in self.layers:
             x = layer.forward(x)
         return x
+
+
+    def forward(self, x, t):
+        score = self.predict(x)
+        loss = self.loss_layer.forward(score, t)
+        return loss
+
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
