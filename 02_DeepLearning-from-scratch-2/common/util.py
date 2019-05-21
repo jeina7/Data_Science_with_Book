b28c7a91bbc4d221cc46ea41d4781161e15e48ac
jeina7 <jane1231001@gmail.com>
2019-05-21 15:10:17 +0900

    Add: ppmi, visualize_2D

--- 02_DeepLearning-from-scratch-2/common/util.py
+++ 02_DeepLearning-from-scratch-2/common/util.py
@@ -4,9 +4,12 @@
 import numpy as np
 
 def preprocess(text):
+    # 전처리
     text = text.lower()
     text = text.replace('.', ' .').replace(',', '').replace("'", ' ').replace('?', ' ?')
     words = text.split(' ')
+
+    # 딕셔너리에 담기
     word_to_id = {}
     id_to_word = {}
     for word in words:
@@ -15,6 +18,7 @@ def preprocess(text):
             word_to_id[word] = new_id
             id_to_word[new_id] = word
 
+    # 단어 ID로 구성된 말뭉치 행렬
     corpus = np.array([word_to_id[w] for w in words])
 
     return corpus, word_to_id, id_to_word
@@ -25,6 +29,7 @@ def create_co_matrix(corpus, window_size=1):
     vocab_size = len(set(corpus))
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
 
+    # 모든 단어를 조사하면서 주변 단어들에 대한 개수 세기
     for idx, word_id in enumerate(corpus):
         for i in range(1, window_size+1):
             left_idx = idx - i
@@ -75,3 +80,36 @@ def most_similar(word, word_to_id, id_to_word, co_matrix, top=5):
         count += 1
         if count >= top:
             return
+
+
+def ppmi(co_matrix, verbose=False, eps=1e-8):
+    PPMI_matrix = np.zeros_like(co_matrix, dtype=np.float32)
+
+    # 모든 단어의 발생 빈도수 총합
+    N = np.sum(co_matrix)
+
+    # 각 단어의 독립 발생 빈도 행렬
+    S = np.sum(co_matrix, axis=0)
+
+    total_count = co_matrix.shape[0] * co_matrix.shape[1]
+    count = 0
+
+    for i in range(co_matrix.shape[0]):
+        for j in range(co_matrix.shape[1]):
+            pmi = np.log2(co_matrix[i, j] * N / (S[i] * S[j]) + eps)
+            PPMI_matrix[i, j] = max(0, pmi)
+
+            if verbose:
+                count += 1
+                if (count+1) % (total_count // 20) == 0:
+                    print("%.1f%% Done" % (count / total_count * 100))
+
+    return PPMI_matrix
+
+
+def visualize_2D(U, word_to_id):
+    plt.figure(figsize=(16, 10))
+    for word, word_id in word_to_id.items():
+        plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
+    plt.scatter(U[:, 0], U[:, 1], alpha=0.5)
+    plt.show()
