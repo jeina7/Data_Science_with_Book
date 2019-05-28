8e0b6fe8fbf648ea7a24d42c67c4b5f4b1e9bf18
jeina7 <jane1231001@gmail.com>
2019-05-28 15:38:42 +0900

    Add: SimpleCBOW

--- 02_DeepLearning-from-scratch-2/common/net.py
+++ 02_DeepLearning-from-scratch-2/common/net.py
@@ -5,7 +5,7 @@
 # 2) 모든 Net은 그 안에 포함된 layers의 params를 모두 합친 Net의 새로운 변수 params를 가진다
 
 import numpy as np
-from .layer import Sigmoid, Affine, SoftmaxWithLoss
+from .layer import Sigmoid, Affine, SoftmaxWithLoss, MatMul
 
 
 class TwoLayerNet:
@@ -52,3 +52,46 @@ class TwoLayerNet:
         for layer in reversed(self.layers):
             dout = layer.backward(dout)
         return dout
+
+
+class SimpleCBOW:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+
+        # 가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H, V).astype('f')
+
+        # 계층 생성
+        self.in_layer0 = MatMul(W_in)
+        self.in_layer1 = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
+
+        # 가중치와 기울기 한 리스트에 모으기
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 단어의 분산벡터는 in 가중치로 이용
+        self.word_vecs = W_in
+
+
+    def forward(self, contexts, target):
+        h0 = self.in_layer0.forward(contexts[:, 0])
+        h1 = self.in_layer1.forward(contexts[:, 1])
+        h = (h0 + h1) / 2
+        score = self.out_layer.forward(h)
+        loss = self.loss_layer.forward(score, target)
+        return loss
+
+
+    def backward(self, dout=1):
+        ds = self.loss_layer.backward(dout)
+        da = self.out_layer.backward(ds)
+        da *= 0.5
+        self.in_layer0.backward(da)
+        self.in_layer1.backward(da)
+        return None
