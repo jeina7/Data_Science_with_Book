833d47f949cd9d35260cacc6a188ab5a99910a61
jeina7 <jane1231001@gmail.com>
2019-08-08 19:43:41 +0900

    [add] SigmoidWithLoss, Embedding, NegativeSamplingLoss

--- 02_DeepLearning-from-scratch-2/common/layer.py
+++ 02_DeepLearning-from-scratch-2/common/layer.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 from .function import softmax, cross_entropy_error
+from .util import UnigramSampler
 
 
 class Sigmoid:
@@ -74,6 +75,28 @@ class MatMul:
         return dx
 
 
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - sefl.t) * dout / batch_size
+        return dx
+
+
 class SoftmaxWithLoss:
     def __init__(self):
         self.params = []
@@ -103,3 +126,95 @@ class SoftmaxWithLoss:
         dx *= dout
         dx = dx / batch_size
         return dx
+
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+
+        # dW의 모든 값을 0으로 덮어씌우기
+        dW[...] = 0
+
+        # idx에 해당하는 행에 dout값을 모두 더해주기
+        np.add.at(dW, self.idx, dout)
+        return None
+
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+
+        # dot 연산에 대한 backward
+        # dout의 값에 각각 서로를 바꿔서 곱해준 값이 gradient값
+        dtarget_W = dout * h
+        dh = dout * target_W
+        # embed 계층 쪽에도 backward 수행
+        self.embed.backward(dtarget_W)
+        return dh
+
+
+class NegativeSamplingLoss:
+    def __init__(self, W, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+
+        # negative sample size에 정답 sample을 위해 1을 더해줌
+        self.loss_layers = [sigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+
+        self.params, self.grads = [], []
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        # Positive sample
+        score = self.embed_dot_layers[0].forward(h, target)
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+
+        # Negative sample
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[1+i].forward(h, negative_target)
+            loss += self.loss_layers[1+i].forward(score, negative_label)
+
+        return loss
+
+
+    def backward(self, dout=1):
+        dh = 0
+        for loss_layer, embed_dot_layer in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = loss_layer.backward(dout)
+            dh += embed_dot_layer.backward(dscore)
+
+        return dh
+        
