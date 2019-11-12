0769811d8c0e004fff0a04d797d574981833f602
jeina7 <jane1231001@gmail.com>
2019-11-12 23:37:18 +0900

    [new] CBOW model

--- 02_DeepLearning-from-scratch-2/common/net.py
+++ 02_DeepLearning-from-scratch-2/common/net.py
@@ -4,8 +4,9 @@
 # 1) 모든 Net은 함수 predict()를 가진다
 # 2) 모든 Net은 그 안에 포함된 layers의 params를 모두 합친 Net의 새로운 변수 params를 가진다
 
-import numpy as np
+from .np import *
 from .layer import Sigmoid, Affine, SoftmaxWithLoss, MatMul
+from .layer import Embedding, NegativeSamplingLoss
 
 
 class TwoLayerNet:
@@ -94,4 +95,47 @@ class SimpleCBOW:
         da *= 0.5
         self.in_layer0.backward(da)
         self.in_layer1.backward(da)
+        return None
+
+
+class CBOW:
+    def __init__(self, vocab_size, hidden_size, window_size, corpus):
+        V, H = vocab_size, hidden_size
+
+        # 가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(V, H).astype('f')
+
+        # 계층 생성
+        self.in_layers = []
+        for i in range(2 * window_size):
+            layer = Embedding(W_in)
+            self.in_layers.append(layer)
+        self.ns_loss = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
+        layers = self.in_layers + [self.ns_loss]
+
+        # 모든 가중치와 기울기 모으기
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 단어의 분산표현 저장
+        self.word_vecs = W_in
+
+
+    def forward(self, contexts, target):
+        h = 0
+        for i, layer in enumerate(self.in_layers):
+            h += layer.forward(contexts[:, i])
+        h *= 1 / len(self.in_layers)
+        loss = self.ns_loss.forward(h, target)
+        return loss
+
+
+    def backward(self, dout=1):
+        dout = self.ns_loss.backward(dout)
+        dout *= 1 / len(self.in_layers)
+        for layer in self.in_layers:
+            layer.backward(dout)
         return None
