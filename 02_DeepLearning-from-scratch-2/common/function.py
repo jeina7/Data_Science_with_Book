29ff750258c3fd3023321d1186e4257ea812c42b
jeina7 <jane1231001@gmail.com>
2019-11-13 17:29:44 +0900

    [edit] typo

--- 02_DeepLearning-from-scratch-2/common/function.py
+++ 02_DeepLearning-from-scratch-2/common/function.py
@@ -18,6 +18,7 @@ def softmax(x):
         x = np.exp(x)
         x /= x.sum(axis=1, keepdims=True)
     elif x.ndim == 1:
+        x = x - np.max(x)
         x = np.exp(x) / np.sum(np.exp(x))
     return x
 
